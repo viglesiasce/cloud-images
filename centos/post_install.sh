4e28972b4f4f579ee3430d4087a3a48425e95e06
Vic Iglesias <viglesiasce@gmail.com>
2014-06-04 12:31:16 -0700

    Let virt-sysprep take care of udev rules

--- centos/post_install.sh
+++ centos/post_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -rf /etc/udev/rules.d/70-persistent-net.rules
 yum install -y http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm
 yum install -y cloud-init
 passwd -l root
