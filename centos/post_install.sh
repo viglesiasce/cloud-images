5b2f66a494c18969e1d77afbffdb634d584e803e
Vic Iglesias <viglesiasce@gmail.com>
2014-06-04 23:35:38 -0700

    Make root user the default

--- centos/post_install.sh
+++ centos/post_install.sh
@@ -2,3 +2,9 @@
 yum install -y http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm
 yum install -y cloud-init
 passwd -l root
+
+### Configure cloud-init for root
+sed -i 's/disable_root:.*/disable_root: 0/g' /etc/cloud/cloud.cfg
+sed -i 's/cloud-user/root/g' /etc/cloud/cloud.cfg
+
+cat /etc/cloud/cloud.cfg
