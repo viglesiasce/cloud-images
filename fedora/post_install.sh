a9423ce741c0b4c5eb0a37e2e9b95fdbf77584ee
Vic Iglesias <viglesiasce@gmail.com>
2014-06-08 23:08:15 -0700

    Initial commit of Fedora 20

---
+++ fedora/post_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum install -y cloud-init
+passwd -l root
+
+### Configure cloud-init for root
+sed -i 's/disable_root:.*/disable_root: 0/g' /etc/cloud/cloud.cfg
+sed -i 's/cloud-user/root/g' /etc/cloud/cloud.cfg
+
+cat /etc/cloud/cloud.cfg
