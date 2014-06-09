5394331f64a9928243ad3b20f0dca3206979fa94
Vic Iglesias <viglesiasce@gmail.com>
2014-06-08 23:16:19 -0700

    Set proper default user

--- fedora/post_install.sh
+++ fedora/post_install.sh
@@ -4,6 +4,6 @@ passwd -l root
 
 ### Configure cloud-init for root
 sed -i 's/disable_root:.*/disable_root: 0/g' /etc/cloud/cloud.cfg
-sed -i 's/cloud-user/root/g' /etc/cloud/cloud.cfg
+sed -i 's/fedora/root/g' /etc/cloud/cloud.cfg
 
 cat /etc/cloud/cloud.cfg
