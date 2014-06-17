bd5fa804f932b3cfe221068873a0ab1772a3e88b
Vic Iglesias <viglesiasce@gmail.com>
2014-06-16 18:38:08 -0700

    Fix Debian image build process

--- debian-7/post_install.sh
+++ debian-7/post_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-apt-get install cloud-init
-history -c
-apt-get autoremove
-apt-get autoclean
-apt-get clean
-
-#curl https://raw.githubusercontent.com/viglesiasce/cloud-images/master/utils/rc.local > /etc/rc.local
-#chmod +x /etc/rc.local
+curl https://raw.githubusercontent.com/viglesiasce/cloud-images/master/utils/rc.local > /etc/rc.local
+chmod +x /etc/rc.local
 passwd -l root
+passwd -l debian
