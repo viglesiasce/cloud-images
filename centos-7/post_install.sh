663cac488a2fbc791300c8ec5fde091af425cddf
Vic Iglesias <viglesiasce@gmail.com>
2014-07-14 14:03:53 -0700

    Remove second base repo from CentOS 7

--- centos-7/post_install.sh
+++ centos-7/post_install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-cat > /etc/yum.repos.d/base.repo <<EOF
-[base]
-name=base
-baseurl=http://buildlogs.centos.org/centos/7/os/x86_64-20140614/
-gpgcheck=0
-EOF
 yum install -y http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm
 yum install -y cloud-init
 
