aa0b7e0617a9c97c923565b345fe7137d46434df
ahmari6 <ahma@markohelenius.fi>
2015-06-01 10:34:48 +0300

    Update post_install.sh
    For new CentOS 6.6 cloud images we need to change few things for post_install to work:
     * disable firewall and SELinux extension
     * install Java 7

--- micro-qa/post_install.sh
+++ micro-qa/post_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# TFUU
+setenforce 0
+
+# Disable Firewall
+service iptables save
+service iptables stop
+chkconfig iptables off
+
 ### Setup EPEL and install cloud-init
 yum update -y
 yum install -y http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm
@@ -12,7 +21,7 @@ sed -i 's/cloud-user/root/g' /etc/cloud/cloud.cfg
 cat /etc/cloud/cloud.cfg
  
 ### Setup microQA: Source https://gist.github.com/viglesiasce/5847752
-yum install -y python-devel python-setuptools gcc make python-virtualenv java-1.6.0-openjdk.x86_64 git ntp wget unzip ipython
+yum install -y python-devel python-setuptools gcc make python-virtualenv java-1.7.0-openjdk.x86_64 git ntp wget unzip ipython
  
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
@@ -21,8 +30,7 @@ chkconfig jenkins on
  
 chkconfig ntpd on
 service ntpd start
- 
-service jenkins start
+
 pushd /var/lib
 git clone https://github.com/eucalyptus/micro-qa.git
 rsync -va micro-qa/jenkins/ jenkins/
